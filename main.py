d81d689a9535a58b113e884f6c14c5985cd47ee2
everm1nd <everm1nd.mail@gmail.com>
2016-01-17 19:06:51 +0100

    fixed int typing error for arguments

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ start = 0
 stop  = 10
 
 if len(sys.argv) > 2:
-    start = sys.argv[1]
-    stop  = sys.argv[2]
+    start = int(sys.argv[1])
+    stop  = int(sys.argv[2])
 
 for n in range(start,stop):
     result = q.enqueue(grab, 'http://www.pornhub.com/video?page=' + str(n+1))
